2b4ebf7b9c6e15dfd58aab7af017b58a8ca39ac4
fazlibeqir <fazlibeqir@gmail.com>
2024-04-18 19:52:54 +0200

    Logo added and nav bar changed

--- Lab2/frontend/src/components/Header/header.js
+++ Lab2/frontend/src/components/Header/header.js
@@ -1,19 +1,52 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import logo from "../../assets/favicon.ico";
 const header = (props) => {
-   return (
-       <header>
-           <nav className="navbar navbar-dark bg-dark">
-               <div className="container">
-                   <div className="d-flex ">
-                          <Link className="nav-link text-white" to={"/books"}>Books</Link>
-                          <Link className="nav-link text-white" to={"/authors"}>Authors</Link>
-                          <Link className="nav-link text-white" to={"/categories"}>Categories</Link>
-                          <Link className="nav-link text-white" to={"/countries"}>Countries</Link>
-                   </div>
-               </div>
-           </nav>
-       </header>
-   )
-}
+    return (
+        <header>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container">
+                    <Link className="navbar-brand" to={"/books"}>
+                        <img src={logo} alt="Logo" className="logo" />
+                    </Link>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#navbarNav"
+                        aria-controls="navbarNav"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <Link className="nav-link" to={"/books"}>
+                                    Books
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to={"/authors"}>
+                                    Authors
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to={"/categories"}>
+                                    Categories
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to={"/countries"}>
+                                    Countries
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+};
 export default header;
